0879f3705aabaf2b8c1a74efbe904ddec1bf675a
Davaille-Leroy Matisse <mat.dl17000@gmail.com>
2021-12-30 17:03:28 +0100

    MongoDB connection OK

--- back/app.js
+++ back/app.js
@@ -1,6 +1,17 @@
 // Importation d'express
 const express = require('express');
 
+// Importation + paramètrage Mongoose
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(
+		'mongodb+srv://imat17:vicecity123@cluster0.5alvs.mongodb.net/cluster0?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log('Connexion à MongoDB réussie !'))
+	.catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // Notre application > express
 const app = express();
 
